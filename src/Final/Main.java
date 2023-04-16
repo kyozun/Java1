26dd67a64e29cc0cee9e3ab4a985a37c9ba0a5e9
kyozun <daicacuong0311@gmail.com>
2023-04-16 22:16:16 +0700

    HomeWork

--- src/Final/Main.java
+++ src/Final/Main.java
@@ -2,6 +2,6 @@ package Final;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello cuong`!");
+        System.out.println("Hello cuong!");
     }
 }
